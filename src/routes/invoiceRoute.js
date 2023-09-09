b8bb3ec9f6165c13b5964d6224fdfcf83683999d
Aaqib-kk <aqib.khan.25@iiee.edu.pk>
2023-09-09 14:44:53 +0500

    Added: token in blogs route

--- src/routes/invoiceRoute.js
+++ src/routes/invoiceRoute.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const { getInvoice } = require('../controller/invoiceController');
 const router = express.Router();
+const {
+    verifyToken,
+    verifyTokenAndAdmin,
+    verifyTokenAndCorporate,
+  } = require('../middleware/verifytokens');
 
-router.route('/getinvoice').get(getInvoice);
+router.route('/getinvoice').get(verifyToken, getInvoice);
 
 module.exports = router;
