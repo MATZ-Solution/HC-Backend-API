0e5e7c05934af7c64bc802334830ae728fd27695
Muhammad Naeem <64369381+MuhammadNaeemShaikh@users.noreply.github.com>
2023-09-20 10:10:46 +0500

    modify rate limit in one minutes only 10 request can made from each user

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ databaseConnection.connect();
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 15,
+  max: 10,
   message: 'Too many requests from this IP, please try again later.',
 });
 
