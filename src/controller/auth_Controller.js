ce6420ee9a23a49fe5d8bdf75a2fd696a436b08e
MFahad11 <fahadahad567@gmail.com>
2024-06-25 16:16:09 +0500

    email change to official

--- src/controller/auth_Controller.js
+++ src/controller/auth_Controller.js
@@ -603,13 +603,13 @@ var OAuth2 = google.auth.OAuth2;
 // const nodemailer = require('nodemailer');
 
 const oauth2Client = new OAuth2(
-  '314005293340-9eh88g6318enm271d5ti60538lfsr43k.apps.googleusercontent.com',
-  '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',
+  '339736576493-6f2m4bhr51oddu81foqnvqema7a34d0t.apps.googleusercontent.com',
+  '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',
   'https://developers.google.com/oauthplayground'
 );
 oauth2Client.setCredentials({
   refresh_token:
-    '1//04AOWCmqvdobSCgYIARAAGAQSNwF-L9Ire2LiZhFn3OUK8x00E38RLeEbC37vCdhtQAwcpXq0Sc5B8lKDgqxn5-bdcYd6wSG_fv0',
+    '1//04ZPgPMNGNQdBCgYIARAAGAQSNwF-L9Ir030qDFXi9jl8D4aJIjeiZ3IftGjGxUTQ4MRSpTlBaX0CLEHBUThEcY7ylx9GFUzhMlw',
 });
 
 const createTransporter = async () => {
@@ -628,13 +628,13 @@ const createTransporter = async () => {
       service: 'gmail',
       auth: {
         type: 'OAuth2',
-        user: 'maazurrehman42@gmail.com',
+        user: 'healthcareinfoamerican@gmail.com',
         accessToken,
         clientId:
-          '314005293340-9eh88g6318enm271d5ti60538lfsr43k.apps.googleusercontent.com',
-        clientSecret: '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',
+          '339736576493-6f2m4bhr51oddu81foqnvqema7a34d0t.apps.googleusercontent.com',
+        clientSecret: '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',
         refreshToken:
-          '1//04AOWCmqvdobSCgYIARAAGAQSNwF-L9Ire2LiZhFn3OUK8x00E38RLeEbC37vCdhtQAwcpXq0Sc5B8lKDgqxn5-bdcYd6wSG_fv0',
+          '1//04ZPgPMNGNQdBCgYIARAAGAQSNwF-L9Ir030qDFXi9jl8D4aJIjeiZ3IftGjGxUTQ4MRSpTlBaX0CLEHBUThEcY7ylx9GFUzhMlw',
       },
       tls: {
         rejectUnauthorized: false, // Add this line to disable certificate verification
