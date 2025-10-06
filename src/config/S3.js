795d594e4ead3a5e68d7032b5efb5b31210cb58a
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2025-10-06 10:24:15 +0500

    Stop tracking .env file

---
+++ src/config/S3.js
@@ -0,0 +1,10 @@
+
+import dotenv from "dotenv";
+dotenv.config();
+
+module.exports = {
+    awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+    bucketName: process.env.AWS_BUCKET_NAME,
+}
