144505a167249215300506033bd28dffe126fd3b
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2025-10-06 15:31:34 +0500

    change url

--- src/utils/db.js
+++ src/utils/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const connect = () => {
     mongoose
         .connect(process.env.MONGO_URL)
-        .then(() => console.log("DB Connection Successfull!"))
+        .then(() => console.log("DB Connection Successfull!!!"))
         .catch((err) => {
             console.log(err);
         });
